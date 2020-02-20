c258eea008f90dec9d3c2a2dfafefe58b796caa2
typhonier <r.guilcas@outlook.com>
2020-02-20 09:46:19 +0100

    Update prepare_csv.py

--- cold_pulses/prepare_csv.py
+++ cold_pulses/prepare_csv.py
@@ -15,37 +15,38 @@ import pandas as pd
 import numpy as np
 from cold_pulses.config_file import config_data
 
-depths = config_data['depths']
-input_folder = config_data['input_folder']
-time_file_name = config_data['time_file_name']
-name_nc_file = config_data['name_nc_file']
+def prepare_csv():
+  depths = config_data['depths']
+  input_folder = config_data['input_folder']
+  time_file_name = config_data['time_file_name']
+  name_nc_file = config_data['name_nc_file']
 
-LIST_FILES = os.listdir(input_folder)
-if len(LIST_FILES) != len(depths.keys()):
-    print('Wrong depths in config_file.py.')
-    print('Please indicate one depth for each file in the input folder.')
-else:
-    TIME_FILE = pd.read_csv('%s/%s'%(input_folder, time_file_name))
-    TIME_FILE.columns = ['time', 'temperature']
-    TIME_FILE.index = pd.DatetimeIndex(TIME_FILE.time)
-    DATA_DATAFRAME = pd.DataFrame()
-    for key in depths:
-        file = pd.read_csv('%s/%s'%(input_folder, key))
-        file.columns = ['time', 'temperature']
-        file.index = pd.DatetimeIndex(file.time)
-        if key != time_file_name:
-            interpolated_data = np.interp(TIME_FILE.index,
-                                          file.index,
-                                          file.temperature,
-                                          left=np.nan,
-                                          right=np.nan)
-            DATA_DATAFRAME[depths[key]] = interpolated_data
-        else:
-            DATA_DATAFRAME[depths[key]] = file.temperature
-    DATA_DATAFRAME = DATA_DATAFRAME.sort_index(axis=1, ascending=False)
-    DATA_DARRAY = xr.DataArray(DATA_DATAFRAME.values.T,
-                               dims=['depth', 'time'],
-                               coords={'depth':DATA_DATAFRAME.columns,
-                                       'time':DATA_DATAFRAME.index})
-    DATA_DARRAY.name = 'temperature'
-    DATA_DARRAY.to_netcdf('%s/%s.nc'%(input_folder, name_nc_file))
+  LIST_FILES = os.listdir(input_folder)
+  if len(LIST_FILES) != len(depths.keys()):
+      print('Wrong depths in config_file.py.')
+      print('Please indicate one depth for each file in the input folder.')
+  else:
+      TIME_FILE = pd.read_csv('%s/%s'%(input_folder, time_file_name))
+      TIME_FILE.columns = ['time', 'temperature']
+      TIME_FILE.index = pd.DatetimeIndex(TIME_FILE.time)
+      DATA_DATAFRAME = pd.DataFrame()
+      for key in depths:
+          file = pd.read_csv('%s/%s'%(input_folder, key))
+          file.columns = ['time', 'temperature']
+          file.index = pd.DatetimeIndex(file.time)
+          if key != time_file_name:
+              interpolated_data = np.interp(TIME_FILE.index,
+                                            file.index,
+                                            file.temperature,
+                                            left=np.nan,
+                                            right=np.nan)
+              DATA_DATAFRAME[depths[key]] = interpolated_data
+          else:
+              DATA_DATAFRAME[depths[key]] = file.temperature
+      DATA_DATAFRAME = DATA_DATAFRAME.sort_index(axis=1, ascending=False)
+      DATA_DARRAY = xr.DataArray(DATA_DATAFRAME.values.T,
+                                 dims=['depth', 'time'],
+                                 coords={'depth':DATA_DATAFRAME.columns,
+                                         'time':DATA_DATAFRAME.index})
+      DATA_DARRAY.name = 'temperature'
+      DATA_DARRAY.to_netcdf('%s/%s.nc'%(input_folder, name_nc_file))
