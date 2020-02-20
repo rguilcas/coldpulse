b689db82af4dea393441107fd1af5cb80e917f84
typhonier <r.guilcas@outlook.com>
2020-02-20 10:13:03 +0100

    Update config_file.py

--- cold_pulses/config_file.py
+++ cold_pulses/config_file.py
@@ -5,22 +5,23 @@ Created on Tue Feb  4 15:29:43 2020
 @author: Robin
 """
 import os
+file = 'config_file.txt'
+def make_config_data(file):
+	file = open(file)
+	data = file.read().splitlines()
+	data = [line.split(':') for line in data]
+	current_dir = os.getcwd()
+	config_data = dict()
+	for k in [0,1,2,3,5,6,8,9]:
+		if k in [3]:
+	    	config_data[data[k][0]] = '%s\\%s'%(current_dir,data[k][1] 
+		else:
+		config_data[data[k][0]] = data[k][1]
 
-file = open('config_file.txt')
-data = file.read().splitlines()
-data = [line.split(':') for line in data]
-current_dir = os.getcwd()
-config_data = dict()
-for k in [0,1,2,3,5,6,8,9]:
-	if k in [3]:
-    	config_data[data[k][0]] = '%s\\%s'%(current_dir,data[k][1] 
-    else:
-    	config_data[data[k][0]] = data[k][1]
+	depths_dic = dict()
+	for k in range(12,len(data)-1):
+		depths_dic[data[k][0]] = int(data[k][1] )
 
-depths_dic = dict()
-for k in range(12,len(data)-1):
-    depths_dic[data[k][0]] = int(data[k][1] )
-
-config_data['depths'] = depths_dic
+	config_data['depths'] = depths_dic
 
 
