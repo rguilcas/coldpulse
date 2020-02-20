1092e0efda679bd89e5eb2281a2b476bef0a3d9f
typhonier <r.guilcas@outlook.com>
2020-02-20 13:59:07 +0100

    upload newfiles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cold_pulses
- Detection of cold pulses
+ Detection of cold water intrusion in a lowly stratified environment.
+
+ This package allows you to detect accurateley individual old pulses event in a time series over several depths.
+
+ Two files are necessary to use it : one configuration file (config_file.txt) and one script file (cold_pulse_detection.py).
---
+++ required_files/cold_pulse_detection.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 09:17:39 2020
+
+@author: Robin
+"""
+
+#First in command prompt : pip install git+http://github.com/typhonier/cold_pulses
+
+from cold_pulses.main import run
+from cold_pulses.config_file import make_config_data
+from cold_pulses.prepare_csv import prepare_csv
+config_file = make_config_data('config_file.txt')
+prepare_csv_test = bool(config_file['prepare_csv'])
+if prepare_csv_test:
+    prepare_csv()
+run()
+
