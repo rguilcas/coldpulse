3c0e3f54d4c8e931b86a18fe4ee73005f01a0eb7
typhonier <r.guilcas@outlook.com>
2020-02-20 17:02:20 +0100

    Update main.py

--- cold_pulses/main.py
+++ cold_pulses/main.py
@@ -5,41 +5,54 @@ Created on Tue Feb  4 15:34:45 2020
 @author: Robin
 """
 import os
+import warnings
 import xarray as xr
-from cold_pulses.detect_pulse import top_pulse_detect,bot_pulse_detect
+from cold_pulses.detect_pulse import top_pulse_detect, bot_pulse_detect
 from cold_pulses.config_file import make_config_data
 
-import warnings
 warnings.filterwarnings("ignore")
+
 def run():
+    """
+    Main script that should be used to detect cold pulses.
+    """
+    # load config file info
     config_data = make_config_data('config_file.txt')
     input_name = config_data['input_name']
     output_name = config_data['output_name']
     output_dir = config_data['output_dir']
     bot = config_data['bot']
     top = config_data['top']
-        
+    # open netcdf file previously created by processing csv files
     darray = xr.open_dataarray(input_name)
+    # If top or bot are selected, we will compute pulses, and therefore  
+    # create a new outup directory
     if bot or top:
-        dir_name = '%s/%s_pulses_out'%(output_dir,output_name)
+        dir_name = '%s/%s_pulses_out'%(output_dir, output_name)
         os.mkdir(dir_name)
+    # If top or bot are selected, we compute pulses using detection algorithms
+    # in detect_pulses
     if bot:
-        df_bot,ds_bot = bot_pulse_detect(darray)
-        df_bot.to_csv('%s/%s_bot_stats.csv'%(dir_name,output_name))
+        df_bot, ds_bot = bot_pulse_detect(darray)
+        df_bot.to_csv('%s/%s_bot_stats.csv'%(dir_name, output_name))
     if top:
-        df_top,ds_top = top_pulse_detect(darray)
-        df_top.to_csv('%s/%s_top_stats.csv'%(dir_name,output_name))
-        
+        df_top, ds_top = top_pulse_detect(darray)
+        df_top.to_csv('%s/%s_top_stats.csv'%(dir_name, output_name))
+    # If only top or bot is selected, we save the output of the relevant 
+    # detection script as a netcdf file
     if top and not bot:
-        ds_top.to_netcdf('%s/%s_top_data.nc'%(dir_name,output_name))
+        ds_top.to_netcdf('%s/%s_top_data.nc'%(dir_name, output_name))
     if bot and not top:
-        ds_bot.to_netcdf('%s/%s_bot_data.nc'%(dir_name,output_name))
+        ds_bot.to_netcdf('%s/%s_bot_data.nc'%(dir_name, output_name))
+    # If both top and bot are selected, we create a new combined output file
+    # with data from the bot and top scripts outputs and save it as a netcdf 
+    # file
     if top and bot:
-        ds = xr.Dataset()
-        ds['temp'] = ds_top.temp
-        ds['gamma_top'] = ds_top.gamma
-        ds['pulse_temp_top'] = ds_top.pulse_temp
-        ds['gamma_bot'] = ds_bot.gamma
-        ds['pulse_temp_bot'] = ds_bot.pulse_temp
-        ds.to_netcdf('%s/%s_all_data.nc'%(dir_name,output_name))
+        output_dataset = xr.Dataset()
+        output_dataset['temp'] = ds_top.temp
+        output_dataset['gamma_top'] = ds_top.gamma
+        output_dataset['pulse_temp_top'] = ds_top.pulse_temp
+        output_dataset['gamma_bot'] = ds_bot.gamma
+        output_dataset['pulse_temp_bot'] = ds_bot.pulse_temp
+        output_dataset.to_netcdf('%s/%s_all_data.nc'%(dir_name, output_name))
         
