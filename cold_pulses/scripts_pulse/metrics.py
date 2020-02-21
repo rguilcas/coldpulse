5bf2d729a0396d734800521697beedd7d5affaa1
rguilcas <r.guilcas@outlook.com>
2020-02-21 17:45:52 +0100

    Update metrics.py

--- cold_pulses/scripts_pulse/metrics.py
+++ cold_pulses/scripts_pulse/metrics.py
@@ -102,6 +102,8 @@ def get_gamma(start, end, darray, dt,
     moving_start_list = np.where(diff_extracted_darray < 0)[0]
     if moving_start_list.size == 0:
         moving_end = end
+        start_depth = []
+        end_depth = []
     else:
         moving_start = moving_start_list[0]
         moving_end_list = np.where(extracted_darray[moving_start+1:] \
