09325bcf7d8281297f70e12b450dfd39af3ce77b
Robin Guillaume-Castel <robin.guillaume@polytechnique.edu>
2020-04-21 12:05:22 +0200

    Create run_TSI.py

---
+++ run_TSI.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 12:01:44 2020
+
+@author: rguil
+"""
+
+import cold_pulses.pulse_detection as detect
+
+input_dir = 'test_dir'
+
+detect.upwelling_cold_pulses_detection(input_dir)
