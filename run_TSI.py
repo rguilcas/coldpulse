1016ca3700007a05813e247469e851643cb47693
rguilcas <r.guilcas@outlook.com>
2020-06-17 09:45:46 +0200

    Update run_TSI.py

--- run_TSI.py
+++ run_TSI.py
@@ -6,7 +6,13 @@ Created on Tue Apr 21 12:01:44 2020
 """
 
 import cold_pulses.pulse_detection as detect
+import os
 
-input_dir = 'test_dir'
+list_dirs = os.listdir()
+list_dirs.remove('run_TSI.py')
+list_dirs.remove('NCEP-GODAS_ocean-temp_1980-2020.nc')
 
-detect.upwelling_cold_pulses_detection(input_dir)
+for dir_name in list_dirs:
+    if dir_name[-3:]!='out':
+        print(dir_name)
+        detect.upwelling_cold_pulses_detection(dir_name,auto_in=True,ignore_double=True)
