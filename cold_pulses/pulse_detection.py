30c6a9fa26bde889da8ec20152d3e1aafbe955a3
Robin Guillaume-Castel <robin.guillaume@polytechnique.edu>
2020-06-26 16:28:05 +0200

    Update pulse_detection.py

--- cold_pulses/pulse_detection.py
+++ cold_pulses/pulse_detection.py
@@ -263,6 +263,7 @@ def make_tsi_threshold_from_climatology(darray, lon, lat):
         godas_ocean_temp = xr.open_dataarray('NCEP-GODAS_ocean-temp_1980-2020.nc')
     except:
         godas_ocean_temp = xr.open_dataarray('NCEP-GODAS_ocean-temp_1980-2020.nc').pottmp
+        godas_ocean_temp['time'] = pd.date_range('1980-01',freq='m',periods=godas_ocean_temp.time.size)
     
     local_temp = godas_ocean_temp.sel(lon=lon,lat=lat,method='nearest').interp(depth=darray.depth)
     phi = compute_temperature_stratification_index(local_temp)
